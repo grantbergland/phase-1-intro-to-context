ca3914a2f7ce309116500aff20ac650f8c25ac80
grantbergland <grantbergland@gmail.com>
2022-08-31 22:12:23 -0500

    commit tree 8a45a29934d9ed8ce65be17d706f7e4f20d9a595

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function CreateEmployeeRecord(){
+    let employee = {
+    firstName: "name",
+    familyName: "lastName",
+    title: "title",
+    payPerHour: "rate",
+    timeInEvents: "timeIn",
+    timeOutEvents: "timeOut"
+    };
+};
+console.log(employee)
